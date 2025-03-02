491f6a78e1acb25e1543d4c386b298dcc941f49f
smallnews <1371606921@qq.com>
2025-03-02 17:41:10 +0800

    userable

---
+++ src/Traits/Userable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Wsmallnews\User\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Wsmallnews\Order\Models\Order;
+use Wsmallnews\Order\Models\OrderItem;
+use Wsmallnews\User\Models\Address;
+
+trait Userable
+{
+    public function addresses(): MorphMany
+    {
+        return $this->morphMany(Address::class, 'user');
+    }
+
+
+    public function orders(): MorphMany
+    {
+        return $this->morphMany(Order::class, 'buyer');
+    }
+
+
+    public function orderItems(): MorphMany
+    {
+        return $this->morphMany(OrderItem::class, 'buyer');
+    }
+}
