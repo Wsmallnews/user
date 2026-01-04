710b493f17bbce6e63bf8709a613b0c7f8f5c732
smallnews@qq.com <1371606921@qq.com>
2026-01-04 19:01:42 +0800

    用户字段，不存在才创建，avatar_url

--- database/migrations/2025_01_20_113724_add_user_columns_to_users_table.php
+++ database/migrations/2025_01_20_113724_add_user_columns_to_users_table.php
@@ -11,14 +11,48 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->nullable()->unique()->after('name')->comment('用户名');
-            $table->string('mobile', 20)->nullable()->unique()->after('username')->comment('手机号');
-            $table->string('avatar')->nullable()->after('mobile')->comment('头像');
-            $table->tinyInteger('gender')->default(0)->after('avatar_url')->comment('性别:1=男,2=女,0=未知');
-            $table->string('birthday')->nullable()->after('gender')->comment('生日');
-            $table->string('status', 20)->default('normal')->after('remember_token')->comment('状态:normal=正常,disabled=禁用');
-        });
+        Schema::whenTableDoesntHaveColumn(
+            'users', 
+            'username',
+            function (Blueprint $table) {
+                $table->string('username')->nullable()->unique()->after('name')->comment('用户名');
+            }
+        );
+        Schema::whenTableDoesntHaveColumn(
+            'users', 
+            'mobile',
+            function (Blueprint $table) {
+                $table->string('mobile', 20)->nullable()->unique()->after('username')->comment('手机号');
+            }
+        );
+        Schema::whenTableDoesntHaveColumn(
+            'users', 
+            'avatar_url',
+            function (Blueprint $table) {
+                $table->string('avatar_url')->nullable()->after('mobile')->comment('头像');
+            }
+        );
+        Schema::whenTableDoesntHaveColumn(
+            'users',
+            'gender',
+            function (Blueprint $table) {
+                $table->tinyInteger('gender')->default(0)->after('avatar_url')->comment('性别:1=男,2=女,0=未知');
+            }
+        );
+        Schema::whenTableDoesntHaveColumn(
+            'users',
+            'birthday',
+            function (Blueprint $table) {
+                $table->string('birthday')->nullable()->after('gender')->comment('生日');
+            }
+        );
+        Schema::whenTableDoesntHaveColumn(
+            'users',
+            'status',
+            function (Blueprint $table) {
+                $table->string('status', 20)->default('normal')->after('remember_token')->comment('状态:normal=正常,disabled=禁用');
+            }
+        );
     }
 
     /**
@@ -26,15 +60,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'username',
-                'mobile',
-                'avatar',
-                'gender',
-                'birthday',
-                'status',
-            ]);
-        });
+        // 不删字段
     }
 };
