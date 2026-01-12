70d99665596f3227cc81b4136b7d0cb0099b8237
Wsmallnews <16216215+Wsmallnews@users.noreply.github.com>
2026-01-12 10:52:05 +0000

    Fix styling

--- src/Livewire/Components/Settings/Profile.php
+++ src/Livewire/Components/Settings/Profile.php
@@ -2,23 +2,17 @@
 
 namespace Wsmallnews\User\Livewire\Components\Settings;
 
-use Filament\Actions\Action;
 use Filament\Forms\Components;
 use Filament\Schemas\Components\Text;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Schemas\Schema;
-use Filament\Support\Enums\Alignment;
-use Filament\Support\Enums\Width;
-use Filament\Support\Icons\Heroicon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\HtmlString;
-use Illuminate\Validation\Rules\Password as PasswordRule;
 use Livewire\Attributes\Locked;
 use Livewire\Component;
+use Wsmallnews\Support\Support\Utils as SupportUtils;
 use Wsmallnews\User\Facades\UserConfig;
 use Wsmallnews\User\Support\Utils;
-use Wsmallnews\Support\Support\Utils as SupportUtils;
 
 class Profile extends Component implements HasSchemas
 {
